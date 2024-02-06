b0426e35ee0053e1709e42f8241683606a2cdb83
thinktank01 <felixayot@gmail.com>
2024-02-06 16:32:29 +0300

    Upload i18n task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Flask application script."""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def helloworld():
+    """Renders Hello world page."""
+    return render_template("2-index.html", title="Welcome to Holberton")
+
+
+@babel.localeselector
+def get_locale():
+    """Select a language translation."""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5500)
